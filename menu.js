abb03e9780ba179aaaad2da24d74ff4c620326bc
Heitor Zelbato <heitorzelbato@gmail.com>
2024-07-11 16:27:40 -0300

    refazendo portifolio

--- menu.js
+++ menu.js
@@ -1,3 +1,4 @@
+
 let btnMenu = document.getElementById('btn-menu')
 let menu = document.getElementById('menu-mobile')
 let overlay = document.getElementById('overlay-menu')
@@ -12,4 +13,10 @@ menu.addEventListener('click', ()=>{
 
 overlay.addEventListener('click', ()=>{
     menu.classList.remove("abrir-menu")
+})
+
+const chk = document.getElementById('checkbox')
+
+chk.addEventListener('change', () => {
+  document.body.classList.toggle('white')
 })
